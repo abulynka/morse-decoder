3e7139055c95120767d4456cadd4cf59ae555902
Aliaksandr Bulynka <abulynka@gmail.com>
2021-08-12 00:01:24 +0300

    feat: make necessary realization

--- src/index.js
+++ src/index.js
@@ -37,8 +37,37 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+/**
+ * @param {string} expr 
+ */
 function decode(expr) {
-    // write your solution here
+    let morseTable = {};
+    let convert = {
+        '.': '10',
+        '-': '11',
+    };
+
+    for (const [key, value] of Object.entries(MORSE_TABLE)) {
+        let currentKey = '';
+        key.split('').forEach(
+            (e) => {
+                currentKey += convert[e];
+            }
+        );
+        morseTable[currentKey.padStart(10, '0')] = value;
+    }
+
+    let result = '';
+    const arr = expr.split('');
+    for (let i = 0; i < arr.length; i = i + 10) {
+        const char = arr.slice(i, i + 10).join('');
+        if (char === '**********') {
+            result += ' ';
+        } else {
+            result += morseTable[arr.slice(i, i + 10).join('')];
+        }
+    }
+    return result;
 }
 
 module.exports = {
